e8eca05a2e86d8c0941a450cf7f16ee0c22bf82f
Sreekaran Reddy Ramasahayam <sreekaran1704@Sreekarans-Air.lan>
2025-08-07 16:55:17 -0500

    Initial setup is done and installed required dependencies

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sreekaran Reddy",
+    author_email="Sreekaran.2021@gmail.com",
+    packages=find_packages()
+)
